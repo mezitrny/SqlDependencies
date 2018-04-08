39fb8b5d37d2dcd3688023fc4f992a264e7f2a8e
jaroslav <jaroslav.ruzicka@hotmail.com>
2018-04-08 14:02:06 +0200

    add Database

--- SqlAnalyser/SqlAnalyser/DatabaseInfo.cs
+++ SqlAnalyser/SqlAnalyser/DatabaseInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using Microsoft.SqlServer.TransactSql.ScriptDom;
+using RoseByte.AdoSession;
 using SqlAnalyser.Internal;
 using SqlAnalyser.Internal.Identifiers;
 
@@ -10,45 +12,90 @@ namespace SqlAnalyser
     public class DatabaseInfo : IDatabaseInfo
     {
         public string ConnectionString { get; }
-        private SqlConnection Connection { get; set; }
+        private Session Session { get; }
 
+        private string _defaultSchema;
         public string DefaultSchema
         {
-            get { return Connection.GetSchema().ToString(); }
+            get
+            {
+                if (_defaultSchema == null)
+                {
+                    _defaultSchema = Session.GetScalar<string>("SELECT SCHEMA_NAME()"); 
+                    Session.CloseConnection();
+                }
+
+                return _defaultSchema;
+            }
         }
 
         public string DatabaseName { get; }
         public string ServerName { get; }
-        public SqlVersion Version { get; }
+
+        private SqlVersion? _version;
+        public SqlVersion Version
+        {
+            get
+            {
+                if (!_version.HasValue)
+                {
+                    var version = new SqlConnection(ConnectionString).ServerVersion?
+                        .Split('.').FirstOrDefault();
+
+                    switch (version)
+                    {
+                        case "8":
+                            _version = SqlVersion.Sql80;
+                            break;
+                        case "9":
+                            _version = SqlVersion.Sql90;
+                            break;
+                        case "10":
+                            _version = SqlVersion.Sql100;
+                            break;
+                        case "11":
+                            _version = SqlVersion.Sql110;
+                            break;
+                        case "12":
+                            _version = SqlVersion.Sql120;
+                            break;
+                        case "13":
+                            _version = SqlVersion.Sql130;
+                            break;
+                        case "14":
+                            _version = SqlVersion.Sql140;
+                            break;
+                        default:
+                            _version = SqlVersion.Sql140;
+                            break;
+                    }
+                }
+
+                return _version.Value;
+            }
+        }
 
         public DatabaseInfo(string connection)
         {
+            Session = new Session(connection);
             var cb = new SqlConnectionStringBuilder(connection);
             DatabaseName = cb.InitialCatalog;
             ServerName = cb.DataSource;
         }
         
-        public DatabaseInfo(SqlConnection connection)
+        public DatabaseInfo(IDbConnection connection)
         {
             ConnectionString = connection.ConnectionString;
         }
         
-        public DatabaseInfo(SqlVersion version, string defaultSchema, string databaseName, string serverName)
-        {
-            DefaultSchema = defaultSchema;
-            DatabaseName = databaseName;
-            ServerName = serverName;
-            Version = version;
-        }
-        
         public ScriptInfo AnalyseScript(string sql)
         {
-            throw new System.NotImplementedException();
+            return new ScriptInfo(sql, Version, DatabaseName, ServerName, DefaultSchema);
         }
 
-        public ScriptInfo FetchScript(string name, BatchTypes type)
+        public ScriptInfo FetchScript(string name)
         {
-            throw new System.NotImplementedException();
+            return AnalyseScript(Session.GetScalar<string>("OBJECT_DEFINITION(OBJECT_ID('TestProc1'))"));
         }
 
         public IEnumerable<ScriptInfo> FetchScripts(BatchTypes type)
---
+++ SqlAnalyser/SqlAnalyser/ServerInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+
+namespace SqlAnalyser
+{
+    public class ServerInfo
+    {
+        private Scripter _scripter;
+        private Scripter Scripter => _scripter ?? (_scripter = new Scripter(Server));
+        private Server Server { get; }
+
+        public ServerInfo(string server)
+        {
+            Server = new Server(server);
+        }
+
+        DatabaseInfo AnalyzeDatabase(string Database)
+        {
+            
+        }
+    }
+}
